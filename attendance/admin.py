bc37614d4b2c6cec0a1ce5ffd0887ac58256eb46
on1ystar <tjdwls0607@naver.com>
2020-09-25 17:17:29 +0900

    Create models

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Profile, Attendance, Role, Classroom
+from django.conf import settings
+from django.contrib.auth.models import User
+
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['school', 'student_id', 'classroom']
+    list_display_links = ['classroom']
+
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'name', 'desc', 'timer', 'personnel', 'created_at']
+    list_display_links = ['uuid', 'name']
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['user', 'classroom', 'is_attendance', 'time_out', 'checked_at']
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    user = settings.AUTH_USER_MODEL
+    list_display = ['user', 'classroom', 'is_checker']
--- attendance/models.py
+++ attendance/models.py
@@ -2,37 +2,45 @@ from django.conf import settings
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+import uuid
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class User(models.Model):
-    name = models.TextField()
-    # class
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    school = models.CharField(max_length=20)
+    student_id = models.CharField(max_length=20, blank=True)
+    classroom = models.ForeignKey('Classroom', on_delete=models.CASCADE)
 
-class Class(models.Model):
-    # code
-    # timer
-    # name
-    # personnel
+    def __str__(self):
+        return f'{self.user}'
+
+class UserPhoto(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='checkmate/user', blank=True)
+
+
+class Classroom(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    desc = models.TextField(blank=True)
+    timer = models.DurationField(blank=True)
+    personnel = models.PositiveIntegerField(default=10)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
     
 class Role(models.Model):
-    # class
-    # user
-    is_ckecker = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    is_checker = models.BooleanField(default=False)
 
 class Attendance(models.Model):
-    # class
-    # user
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     is_attendance = models.BooleanField(default=False)
+    time_out = models.DurationField(default=0, blank=True)
     checked_at = models.DateTimeField(auto_now_add=True)
-'''
-User-----------------------
-sttings.AUTH_USER_MODEL
------------------------
-
------------------------
-
------------------------
-
-'''

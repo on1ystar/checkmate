11ee1cbda2fad970bc88b5fd998798b38c2a8812
on1ystar <tjdwls0607@naver.com>
2020-10-02 23:34:59 +0900

    settins static

--- checkmate/settings.py
+++ checkmate/settings.py
@@ -127,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'checkmate', 'static'),
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
+INTERNAL_IPS = ['127.0.0.1']

73a76a8ea388c9805b77e83e48eba05b49d6c581
on1ystar <tjdwls0607@naver.com>
2020-10-03 03:13:05 +0900

    Create root.html and layout

--- checkmate/settings.py
+++ checkmate/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'checkmate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'checkmate', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- checkmate/urls.py
+++ checkmate/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView, RedirectView
+
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='root.html'), name='root'),
     path('admin/', admin.site.urls),
     path('attendance/', include('attendance.urls')),
     path('face/', include('face.urls')),
